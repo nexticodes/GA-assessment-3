8d4ecf3a17e318c4b2753eaa8633017818d8c2b7
Ernest Bruno <erndobruno@gmail.com>
2022-01-21 11:34:48 -0800

    Complete step 3.2 - create a model, migrate, and render message if widgets list is empty.

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Widget
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    widgets = Widget.objects.all()
+    return render(request, 'index.html', {'widgets': widgets})
